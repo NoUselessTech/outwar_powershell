1d266105a216202ec1eeb1e1edc3f48bb2f85f41
Connor Peoples <nouselesstech@pop-os.localdomain>
2022-10-02 22:44:43 -0500

    Added changes for the cookies. onto more header stuff

--- modules/Account.psm1
+++ modules/Account.psm1
@@ -1,3 +1,37 @@
+Function Get-UrlParams {
+    param( $Url )
+    $Params = New-Object PSObject
+    $QueryString = ($Url.split('?'))[1]
+    $Groups = $QueryString.split('&')
+
+    ForEach($Group in $Groups) {
+        $Param,$Value = $Group.split('=')
+        $Params | Add-Member `
+            -MemberType NoteProperty `
+            -Name $Param `
+            -Value $Value
+    }
+
+    Return $Params
+}
+
+Function Set-ToraxCookies {
+    param ( $UrlParams )
+
+    $Keys = $UrlParams | Get-Member -MemberType NoteProperty | Select-Object Name
+
+    ForEach($Key in $Keys) {
+        $KeyName = $Key.Name
+        $Cookie = [System.Net.Cookie]::new($KeyName, $UrlParams.$KeyName)
+        $Global:Session.Cookies.Add('http://outwar.com', $Cookie)
+        $Global:Session.Cookies.Add('http://torax.outwar.com', $Cookie)
+    }
+}
+
+Function Set-ToraxHeader {
+
+}
+
 Function Get-LoginSession() {
     $Username = Read-Host "Username"
     $Password = Read-Host "Password" -MaskInput
@@ -14,7 +48,11 @@ Function Get-LoginSession() {
         -Body $Body `
         -SessionVariable 'Global:Session' `
         -MaximumRedirection 0 `
-        -Method POST
+        -Method POST `
+        -SkipHttpErrorCheck
 
-    Write-Host $Global:Session
+
+    $UrlParams = Get-UrlParams -Url $Global:LoginRequest.Headers.Location
+    Set-ToraxCookies($UrlParams)
+
 }
